d7edc0e5a90fa2d027337ab4096452e2e2fb5a7e
Cashfxau <lukebryan09877@gmail.com>
2026-06-07 23:05:15 +0100

    Create firebase.js
    firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyB1WM6hbhq4dWKdCtHNFJYQoqWw5OYzH-8",
+  authDomain: "tradescryptfx.firebaseapp.com",
+  projectId: "tradescryptfx",
+  storageBucket: "tradescryptfx.firebasestorage.app",
+  messagingSenderId: "577694834419",
+  appId: "1:577694834419:web:fbe44ece1b66a46da1a186"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
